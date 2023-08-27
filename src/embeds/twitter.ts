eb4533c4aa82634ff8d2036f8a6eba6ce59b78c4
Florian Spieß <business@minnced.club>
2023-08-27 14:53:27 +0200

    Use fxtwitter to generate twitter metadata

--- src/embeds/twitter.ts
+++ src/embeds/twitter.ts
@@ -4,48 +4,53 @@ import '../html';
 
 const LINK_REGEX = new RegExp('^https?://(?:www\\.)?(?:twitter|x)\\.com/(?:#!/)?([^/]+)/status(?:es)?/([^/]+)$', 'i');
 
-interface TwitterEmbed {
-    url: string;
-    author_name: string;
-    author_url: string;
-    html: string;
-    width: number;
-    height: number;
-    type: string;
-    cache_age: string;
-    provider_name: string;
-    provider_url: string;
-    version: string;
-}
-
-function getDescription(embed: TwitterEmbed): string | undefined {
-    const html = parseHTML(embed.html);
-    const content = html.querySelector('p');
-
-    if (content) {
-        const description = content.structuredText.trim();
-        const author = embed.author_name;
-
-        return `${description}\n- ${author} on Twitter`;
-    }
-}
-
+const FXTWITTER_HEADERS = {
+    'User-Agent': 'Discordbot 2.0',
+};
 
 export async function twitterLinkEmbed(post: RedditPost, link: string, head: HTMLElement) {
     const result = LINK_REGEX.exec(link);
     const [, username, id] = result ?? [];
 
     if (username && id) {
-        const embed: TwitterEmbed = await fetch(`https://publish.twitter.com/oembed?url=https://twitter.com/${username}/status/${id}`).then(r => r.json());
+        const html = await fetch(`https://fxtwitter.com/${username}/status/${id}`, { headers: FXTWITTER_HEADERS }).then(r => r.text()).then(parseHTML);
 
-        const description = getDescription(embed);
-        if (description) {
-            head.meta('twitter:card', 'summary');
-            head.meta('og:description', description);
-            head.meta('twitter:description', description);
+        const description = html.querySelector('meta[property="og:description"]')?.getAttribute('content');
+        const image = html.querySelector('meta[property="og:image"]')?.getAttribute('content');
+        const video = html.querySelector('meta[property="og:video"]')?.getAttribute('content');
+        const title = html.querySelector('meta[property="og:title"]')?.getAttribute('content');
+        const card = html.querySelector('meta[property="twitter:card"]')?.getAttribute('content');
+
+        if (card) {
+            head.meta('twitter:card', card);
         }
-        if (embed.author_name) {
-            head.meta('twitter:creator', embed.author_name);
+
+        if (description) {
+            const richText = `${description}\n- ${title} on Twitter`;
+            head.meta('og:description', richText);
+            head.meta('twitter:description', richText);
+        } else {
+            head.meta('og:description', `${title} on Twitter`);
+            head.meta('twitter:description', `${title} on Twitter`);
+        }
+
+        if (image) {
+            const width = toNum(html.querySelector('meta[property="og:image:width"]')?.getAttribute('content'));
+            const height = toNum(html.querySelector('meta[property="og:image:height"]')?.getAttribute('content'));
+
+            head.image(image, width, height);
+        }
+
+        if (video) {
+            const type = html.querySelector('meta[property="og:video:type"]')?.getAttribute('content') ?? 'video/mp4';
+            const width = toNum(html.querySelector('meta[property="og:video:width"]')?.getAttribute('content'));
+            const height = toNum(html.querySelector('meta[property="og:video:height"]')?.getAttribute('content'));
+
+            head.video(video, width, height, type);
         }
     }
+}
+
+function toNum(str: string | null | undefined) {
+    return str ? parseInt(str) : undefined;
 }
