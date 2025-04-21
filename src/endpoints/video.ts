5dd0b1c64fbc5e328d541eee27cf0be141f36973
Florian Spieß <business@minn.dev>
2025-04-21 17:16:50 +0200

    Fix video url parsing

--- src/endpoints/video.ts
+++ src/endpoints/video.ts
@@ -1,16 +1,23 @@
 import { get_packaged_video, redirectPage } from '../util';
 import { IRequest } from 'itty-router';
 
+const notFound = new Response(undefined, {
+    status: 404
+});
+
 export async function getVideo(request: IRequest) {
-    const path = request.url.split('/v')[1];
+    const url = new URL(request.url);
+    if (!url.pathname.startsWith('/v/')) {
+        return notFound;
+    }
+
+    const path = url.pathname.substring('/v'.length);
     const video = await get_packaged_video(path);
     if (video) {
         const videoUrl = video.url;
         const headers = { 'Location': videoUrl, 'content-type': 'text/html' };
         return new Response(redirectPage(videoUrl).toString(), { headers, status: 302 });
     } else {
-        return new Response(undefined, {
-            status: 404
-        });
+        return notFound;
     }
 }
